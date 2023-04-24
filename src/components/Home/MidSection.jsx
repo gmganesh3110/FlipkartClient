d70151bf9b05dd294490592d457b5f5d495584d4
G M Ganesh <gmganesh173@gmail.com>
2023-04-24 23:25:08 +0530

    image changes

--- src/components/Home/MidSection.jsx
+++ src/components/Home/MidSection.jsx
@@ -29,9 +29,9 @@ const MidSection = () => {
   return (
     <>
       <Wrapper lg={12} sm={12} md={12} xs={12} container>
-        {ImageURL.map((image) => (
+        {ImageURL.map((imag) => (
           <Grid item lg={4} md={4} sm={12} xs={12}>
-            <img src={image} style={{ width: "100%" }} />
+            <img src={imag} alt="image1" style={{ width: "100%" }} />
           </Grid>
         ))}
       </Wrapper>
